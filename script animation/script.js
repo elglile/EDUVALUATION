4271b8fc9d3729ba42f00f6de1d6b6df33e6f1b0
Ayoub El-Glile <ayoubelglile@gmail.com>
2026-01-02 17:14:19 +0100

    first commit

---
+++ script animation/script.js
@@ -0,0 +1,10 @@
+
+function show_hide() {  
+    var click = document.getElementById("nav");  
+    if(click.style.display ==="none") {  
+       click.style.display ="flex";  
+    } else {  
+       click.style.display ="none";  
+    }   
+ } 
+
